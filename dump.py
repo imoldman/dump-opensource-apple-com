3f0d31580a2169663f1cf4fbfa109b5a043d8025
lixianliang <lixianliang@corp.netease.com>
2014-01-22 13:24:46 +0800

    modify storage

--- dump.py
+++ dump.py
@@ -7,12 +7,14 @@ import os
 import re
 import sys
 
-URL_ROOT = 'http://www.opensource.apple.com/tarballs'
+URL_ROOT = 'http://www.opensource.apple.com/tarballs/'
 FOLDER_IMG = '/static/images/icons/folder.png'
 GZ_IMG = '/static/images/icons/gz.png'
 BACK_IMG = '/icons/back.gif'
 HREF_RE = re.compile(r'<td[^>]*><a href=\"([^"]+)\"><img[^>]+src="([^"]+)"[^>]*></a></td>')
-RESULT_FILE = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'result.txt'), 'w')
+RESULT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'result')
+TEMP_RESULT_FILE_NAME = 'result.txt.tmp'
+RESULT_FILE_NAME = 'result.txt'
 
 def get_content_from_remote(path):
   print 'downloading content from %s ... ' % path
@@ -27,31 +29,59 @@ def get_tar_gz_file(path):
   content = get_content_from_remote(path)
   return tarfile.open(fileobj=StringIO(content))
 
+def make_sure_dir_exists(path):
+  full_path = os.path.join(RESULT_DIR, path)
+  if not os.path.exists(full_path):
+    os.makedirs(full_path)
+
+def is_gz_parse_complete(path):
+  full_path  = os.path.join(RESULT_DIR, path, RESULT_FILE_NAME)
+  return os.path.exists(full_path)
+
+def get_file_object_to_store_result(path):
+  full_directory = os.path.join(RESULT_DIR, path)
+  make_sure_dir_exists(full_directory)
+  return open(os.path.join(full_directory, TEMP_RESULT_FILE_NAME), 'w')
+
+def mark_gz_parse_complete(path):
+  full_path = os.path.join(RESULT_DIR, path, RESULT_FILE_NAME)
+  temp_file_path = os.path.join(RESULT_DIR, path, TEMP_RESULT_FILE_NAME)
+  os.rename(temp_file_path, full_path)
+
 def dump_gz_file(path):
-  tarfile = get_tar_gz_file(path)
-  names = tarfile.getnames()
-  for name in names:
-    print >> RESULT_FILE, path + '/' + name
+  result_file = get_file_object_to_store_result(path)
+  try:
+    tarfile = get_tar_gz_file(path)
+    names = tarfile.getnames()
+    for name in names:
+      print >> result_file, path + '/' + name
+  except:
+    print >> sys.stderr, 'unable to untar file: %s' % path
 
 def recursive_dump_file_on_html(path):
   content = get_html_content(path)
-  for match in HREF_RE.finditer(content):
-    href = match.group(1)
-    type = match.group(2)
+  href_and_types  = [(m.group(1), m.group(2)) for m in HREF_RE.finditer(content)];
+  total_count = len(href_and_types)
+  for index, href_and_type in enumerate(href_and_types):
+    href = href_and_type[0]
+    type = href_and_type[1]
     if type == BACK_IMG:
       continue			# ignore Parent Directory
+    print '(%d/%d), ' % (index, total_count),
     new_path = path + href
     if type == FOLDER_IMG:
       print 'found type: folder, href: %s => %s' % (path, href)
       recursive_dump_file_on_html(new_path)
     elif type == GZ_IMG:
       print 'found type: tar.gz, href: %s => %s' % (path, href)
-      dump_gz_file(new_path)
+      if not is_gz_parse_complete(new_path):
+        dump_gz_file(new_path)
+        mark_gz_parse_complete(new_path)
     else:
       print >> sys.stderr, 'unkown type: %s, href: %s => %s' % (type, path, href)
 
 def main():
-  recursive_dump_file_on_html('/');
+  recursive_dump_file_on_html('./');
 
 if __name__ == '__main__':
   main()
